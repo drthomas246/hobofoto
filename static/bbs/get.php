964d69d318082e4b62245eb66a1db93e2a5abba5
Dr?Thomas <drthomas@hobofoto.net>
2020-10-14 00:31:58 +0900

    bbsの設定をちゃんとする。

--- static/bbs/get.php
+++ static/bbs/get.php
@@ -6,11 +6,32 @@ if($request !== 'xmlhttprequest') exit;
  
 $parameter = filter_input(INPUT_POST, 'parameter');
 if ($parameter == 'get'){
-	$dat = file('bbs.dat');
+	$dat = file('bbs.dat',FILE_IGNORE_NEW_LINES);
 	$array = array();
 	foreach ($dat as $value){
 		list($name, $mail, $days, $bbs, $extra) = preg_split("[,]",$value, 5);
-		$line = "名前：".$name." メールアドレス：".$mail."</br>\n<date>".$days."</date>\n<p>".$bbs."</p>";
+		$line = "名前：".$name." メールアドレス：".$mail."</br><date>".$days."</date><p>".str_replace(array("\r", "\n"), '', $bbs)."</p>";
+		array_push($array,$line);
+	}
+	$html = implode("\n", $array);
+	header('Content-type: application/json; charset=utf-8');
+	echo json_encode(['data' => $html]);
+}
+if ($parameter == 'send'){
+	$name = filter_input(INPUT_POST, 'name');
+	$mail = filter_input(INPUT_POST, 'mail');
+	$text = filter_input(INPUT_POST, 'text');
+	$w = date("w");
+	$week_name = array("日", "月", "火", "水", "木", "金", "土");
+
+	$line = $name.",".$mail.",".date("Y年m月d日")."（$week_name[$w]）".date("H:i:s").",".str_replace(array("\r", "\n"), '', nl2br($text));
+	$dat = file('bbs.dat',FILE_IGNORE_NEW_LINES);
+	array_unshift($dat,$line);
+	file_put_contents('bbs.dat',implode("\n", $dat));
+	$array = array();
+	foreach ($dat as $value){
+		list($name, $mail, $days, $bbs, $extra) = preg_split("[,]",$value, 5);
+		$line = "名前：".$name." メールアドレス：".$mail."</br><date>".$days."</date><p>".str_replace(array("\r", "\n"), '', $bbs)."</p>";
 		array_push($array,$line);
 	}
 	$html = implode("\n", $array);
--- static/bbs/js/bbs.js
+++ static/bbs/js/bbs.js
@@ -1,7 +1,40 @@
-$(function(){
-    var param = { "parameter": "get" };
-    //var param = "困ったな";
+function sends(){
+    var errorText = "";
+    var error = false;
+    if (!$("#name").val()){
+        errorText = "名前を入れてください。";
+        error = true;
+    }
+    if (!$("#text").val()){
+        errorText = errorText + "本文を入れてください。";
+        error = true;
+    }
+    if (error){
+        $("#bbs-error").html(errorText);
+        return;
+    }else{
+        var name = $("#name").val();
+        var mail = $("#mail").val();
+        var text = $("#text").val();
+        var param = { "parameter": "send" ,"name": name ,"mail": mail ,"text": text };
+        $.ajax({
+            type: "POST",
+            url: "get.php",
+            data: param,
+            crossDomain: false,
+            dataType : "json",
+            scriptCharset: 'utf-8'
+        }).done(function(data){
+            $("#text").val('');
+            prints(1, data.data, false);
+        }).fail(function(XMLHttpRequest, textStatus, errorThrown){
+            alert('正しい結果を得られませんでした。');
+        });
+    }
+}
 
+function onloads(page = 1, buttonClick = false){
+    var param = { "parameter": "get" };
     $.ajax({
         type: "POST",
         url: "get.php",
@@ -10,8 +43,76 @@ $(function(){
         dataType : "json",
         scriptCharset: 'utf-8'
     }).done(function(data){
-        $("#bbs-data").html(data.data);
+        prints(page, data.data, buttonClick);
     }).fail(function(XMLHttpRequest, textStatus, errorThrown){
         alert('正しい結果を得られませんでした。');
     });
-});
+}
+
+function prints(page, data, buttonClick){
+    var dataArray = data.split('\n');
+    var array = [];
+    var kugiri = 10;
+    var pageMax = Math.ceil(dataArray.length / kugiri);
+    var front = page - 2;
+    var back = page + 3;
+    $("#bbs-error").html("");
+    if (page != 1){
+        var value = "<button type='button' onclick='onloads(" + (page-1) + ", true)'>前へ</button>";
+        array.push(value);
+
+    }else{
+        var value = "<button type='button' disabled>前へ</button>";
+        array.push(value);
+    }
+    if (page <= 5){
+        for (var i = 1; i <= 10; i++) {
+            if (page == i){
+                var value = i;
+            }else{
+                var value = "<button type='button' onclick='onloads(" + i + ", true)'>" + i + "</button>";
+            }
+            array.push(value);
+        }
+    }else if(page >=(pageMax-4)){
+        for (var i = (pageMax-9); i <= pageMax; i++) {
+            if (page == i){
+                var value = i;
+            }else{
+                var value = "<button type='button' onclick='onloads(" + i + ", true)'>" + i + "</button>";
+            }
+            array.push(value);
+        }
+    }else{
+        for (var i = (page-5); i <= (page+4); i++) {
+            if (page == i){
+                var value = i;
+            }else{
+                var value = "<button type='button' onclick='onloads(" + i + ", true)'>" + i + "</button>";
+            }
+            array.push(value);
+        }
+    }
+    if (page != pageMax){
+        var value = "<button type='button' onclick='onloads(" + (page+1) + ", true)'>次へ</button>";
+        array.push(value);
+
+    }else{
+        var value = "<button type='button' disabled>次へ</button>";
+        array.push(value);
+    }
+    $(".bbs-link").html(array.join(' '));
+    var array = [];
+    var front = (page - 1) * kugiri;
+    var back = front + kugiri;
+    for (var i = 0; i < dataArray.length; i++) {
+        if (i >= front && i < back){
+            array.push(dataArray[i]);
+        }
+    }
+    $("#bbs-data").html(array.join(''));
+    if (buttonClick){
+        var scroll = $("#scroll").offset();
+        $("body, html").animate({ scrollTop: scroll.top }, 200, "linear");
+    }
+}
